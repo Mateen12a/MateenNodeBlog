6ee3109bf6ae7dc3696cf272ac065924d8a14dd7
mateen james <ajidagbamateen12@gmail.com>
2019-01-08 20:50:46 +0100

    first ccom

---
+++ public/index.ts
@@ -0,0 +1,23 @@
+// start:ng42.barrel
+export * from './css';
+export * from './img';
+export * from './js';
+export * from './posts';
+export * from './vendor';
+export * from './vendor/bootstrap';
+export * from './vendor/bootstrap/css';
+export * from './vendor/bootstrap/js';
+export * from './vendor/fontawesome-free';
+export * from './vendor/fontawesome-free/css';
+export * from './vendor/fontawesome-free/js';
+export * from './vendor/fontawesome-free/less';
+export * from './vendor/fontawesome-free/scss';
+export * from './vendor/fontawesome-free/sprites';
+export * from './vendor/fontawesome-free/svgs';
+export * from './vendor/fontawesome-free/svgs/brands';
+export * from './vendor/fontawesome-free/svgs/regular';
+export * from './vendor/fontawesome-free/svgs/solid';
+export * from './vendor/fontawesome-free/webfonts';
+export * from './vendor/jquery';
+// end:ng42.barrel
+
