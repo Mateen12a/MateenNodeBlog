75704e8ccb0efea2caac05a7ba9d94898a522f01
mateen james <ajidagbamateen12@gmail.com>
2019-01-19 17:40:06 +0100

    lastset

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,11 @@
 const User = require('../database/models/User');
-
+ 
 module.exports = (req, res, next) => {
-    User.findById(req.session.userId, function (error, user){
+    User.findById(req.session.userId, (error, user) => {
         if (error || !user) {
-            return res.redirect('/');
+            return res.redirect('/')
         }
-
+ 
         next();
     });
-};
+}
